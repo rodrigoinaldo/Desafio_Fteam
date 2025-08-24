7f48d2e2a1de577dadfcf20c6388b3559e2d4af9
Rodrigo Alcantara <rodrigoinaldo281@gmail.com>
2025-08-24 14:32:26 -0300

    rate limiting rotas no laravel

--- routes/api.php
+++ routes/api.php
@@ -9,9 +9,15 @@ use Illuminate\Support\Testing\Fakes\Fake;
 
 Route::middleware('integracao')->group(function () {
 
-    Route::get('/fake-store', [FakeStoreController::class, 'allEstoque']);
-
-    Route::get('/produtos', [CatalogController::class, 'index']);
-    Route::get('/produtos/{id}', [CatalogController::class, 'show']);
+    // rate limiting
+    Route::middleware('throttle:1,5')->group(function () {
+        Route::get('/fake-store', [FakeStoreController::class, 'allEstoque']);
+    });
+    
+    // rate limiting
+    Route::middleware('throttle:200,5')->group(function () {
+        Route::get('/produtos', [CatalogController::class, 'index']);
+        Route::get('/produtos/{id}', [CatalogController::class, 'show']);
+    });
 
 });
